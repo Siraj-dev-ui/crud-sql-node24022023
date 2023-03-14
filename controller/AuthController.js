3b33abb5d6fd070015ab7687b35ef159efc39a66
siraj-dev-ui <sirajalig86@gmail.com>
2023-03-14 16:46:39 +0500

    token payload encrypted

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,5 +1,8 @@
 const Db = require('../services/dbOperations');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const { log } = console;
 
 exports.signup = async (req, res) => {
   const { email, password, cnic } = req.body;
@@ -18,31 +21,63 @@ exports.signup = async (req, res) => {
   }
 };
 
+const secret = 'my_secret_key';
+const algorithm = 'aes-256-cbc';
+
+const encrypt = (payload, key) => {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  const encrypted = Buffer.concat([
+    cipher.update(JSON.stringify(payload)),
+    cipher.final(),
+  ]);
+  return {
+    iv: iv.toString('base64'),
+    data: encrypted.toString('base64'),
+  };
+};
+
 exports.login = async (req, res) => {
   try {
     let { email, password } = req.body;
     let query = `select * from signup where Email='${email}' and password='${password}'`;
     const data = await Db.getRecord(query);
     if (data.length > 0) {
+      const encryptionKey = crypto.randomBytes(32);
+      const encryptedPayload = encrypt(data, encryptionKey);
+
+      // encrypted token
+
       const token = jwt.sign(
         {
-          data: { _id: data[0].ID },
+          encrypted_payload: encryptedPayload,
         },
-        process.env.SECRET_KEY,
-        {
-          expiresIn: '2h',
-        }
+        secret,
+        { algorithm: 'HS256' }
       );
 
+      // simpel token
+
+      // const token = jwt.sign(
+      //   {
+      //     data: { _id: data[0].ID },
+      //   },
+      //   process.env.SECRET_KEY,
+      //   {
+      //     expiresIn: '2h',
+      //   }
+      // );
+
       // method 1
-      res.set('Authorization', `Bearer ${token}`);
+      // res.set('Authorization', `Bearer ${token}`);
 
       // method 2
+      // res.header('auth-token', jwe).json({ success: true, payload: data[0] });
       res.header('auth-token', token).json({ success: true, payload: data[0] });
     } else {
       res.json({ success: false, message: 'Incorrect email or password...' });
     }
   } catch (err) {
-    res.json({ success: false, message: 'error while logging in...' });
+    res.json({ success: false, message: err });
   }
 };
